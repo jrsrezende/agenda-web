98b23822fd32221730c6ec0356b8175c105eb960
João Rezende <joaoricardorezende@gmail.com>
2025-08-28 21:06:47 -0300

    feature: pagina de cadastro de tarefas

---
+++ src/app/components/pages/consultar-tarefas/consultar-tarefas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-tarefas',
+  imports: [],
+  templateUrl: './consultar-tarefas.html',
+  styleUrl: './consultar-tarefas.css'
+})
+export class ConsultarTarefas {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'http://localhost:8080';
+
+export const environment = {
+    apiCategories: baseUrl + '/api/v1/categories',
+    apiTasks: baseUrl + '/api/v1/tasks'
+};
+
