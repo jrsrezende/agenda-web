cac29b0ec18cf16f7254ddd78b414e8187483b3d
João Rezende <joaoricardorezende@gmail.com>
2025-09-18 22:01:28 -0300

    feat: add search-tasks page

---
+++ src/app/components/pages/search-tasks/search-tasks.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-search-tasks',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './search-tasks.html',
+  styleUrl: './search-tasks.css',
+})
+export class SearchTasks {
+  tasks = signal<any[]>([]);
+
+  private http = inject(HttpClient);
+
+  formConsulta = new FormGroup({
+    minDate: new FormControl('', [Validators.required]),
+    maxDate: new FormControl('', [Validators.required]),
+  });
+
+  searchTasks() {
+    const dataMin = this.formConsulta.value.minDate;
+    const dataMax = this.formConsulta.value.maxDate;
+    this.http.get(environment.apiTasks + '/' + dataMin + '/' + dataMax).subscribe({
+      next: (response) => {
+        console.table(response);
+        this.tasks.set(response as any[]);
+      },
+      error: (e) => {
+        console.log(e.error);
+      },
+    });
+  }
+
+  deleteTask(id: string) {
+    if (confirm('Do you really want to delete the task?')) {
+      this.http.delete(environment.apiTasks + '/' + id, { responseType: 'text' }).subscribe({
+        next: (response) => {
+          alert(response);
+          this.searchTasks();
+        },
+        error: (e) => {
+          console.log(e.error);
+        },
+      });
+    }
+  }
+}
